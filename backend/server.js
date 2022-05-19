cb9c3268b4ac1edde24d61f6788f9122af2adc23
unknown <tamiloreolaniyi@gmail.com>
2022-05-19 02:06:44 +0100

    mongodb done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const { use } = require('./routes/goalRoutes')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
