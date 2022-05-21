2572e53d790333690ec4eeefb313c6e0b527d816
unknown <tamiloreolaniyi@gmail.com>
2022-05-21 20:07:25 +0100

    protected

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,8 @@ const registerUser = asyncHandler(async(req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -54,7 +55,8 @@ const loginUser = asyncHandler(async(req, res) => {
         res.json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -64,9 +66,24 @@ const loginUser = asyncHandler(async(req, res) => {
 
 //GET user data
 // Get /api/users/me
+//@access private
 const getMe = asyncHandler(async(req, res) => {
-    res.json({ message: 'User data display' })
+    const { _id, name, email } = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
 })
+
+
+//Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
