b70aa53f547742beace62b32d81558f26c860ae3
unknown <tamiloreolaniyi@gmail.com>
2022-05-31 22:01:06 +0100

    front end auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Header from './components/Header'
 import {ToastContainer} from 'react-toastify'
-import 'react=toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <>
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { FaLessThanEqual } from 'react-icons/fa'
 import authService from './authService'
 
 //Get user from localStorage
@@ -10,7 +9,7 @@ const initialState = {
     isError: false,
     isSuccess: false,
     isLoading: false,
-    message: ''
+    message: '',
 }
 
 //Register User
@@ -23,6 +22,21 @@ export const register = createAsyncThunk('auth/register', async(user, thunkAPI) 
     }
 })
 
+//login User
+export const login = createAsyncThunk('auth/login', async(user, thunkAPI) => {
+    try {
+        return await authService.login(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+export const logout = createAsyncThunk('auth/logout', async() => {
+    await authService.logout()
+})
+
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
@@ -31,7 +45,7 @@ export const authSlice = createSlice({
             state.isLoading = false
             state.isSuccess = false
             state.isError = false
-            state.message = false
+            state.message = ''
         }
     },
     extraReducers: (builder) => {
@@ -50,6 +64,24 @@ export const authSlice = createSlice({
                 state.message = action.payload
                 state.user = null
             })
+            .addCase(login.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.user = null
+            })
+
     }
 })
 
