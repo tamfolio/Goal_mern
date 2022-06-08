55e9d78312c6818d4a93c8341624eb3006fe5f23
unknown <tamiloreolaniyi@gmail.com>
2022-06-08 03:11:09 +0100

    final work before styling

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const { use } = require('./routes/goalRoutes')
@@ -15,6 +16,16 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build',
+        'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
